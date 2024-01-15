4b03c751fe378344cc3ad4b26c061e9cffe4f09f
Nikita Rathee <nikirathee24@gmail.com>
2024-01-15 09:36:09 +0530

    made changes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const { connection } = require("./config/db");
 const {userController}=require("./controller/user.controller")
 app.use(express.json());
 app.use(cors({
-      origin:["http://localhost:3000"]
+      origin:["http://localhost:3000","https://lustrous-semifreddo-79ebde.netlify.app"]
 }))
 app.get("/",(req,res)=>{
       res.send("welcome");
